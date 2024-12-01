16e020802c95c074f4708472a16c25eca94e2618
Satya <Trikasatya@Admins-MacBook-Air.local>
2024-12-02 01:11:03 +0530

    Learnt JSX - JSX is HTML like syntax

--- App.js
+++ App.js
@@ -2,5 +2,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = React.createElement('h1', { }, 'Hello, world');
-ReactDOM.createRoot(document.getElementById('root')).render(element);
+const element = React.createElement('h1', {style: {color: "red"}}, 'Hello, world');
+console.log(element);
+
+const JSXElement = (
+    <>
+        <h1>JSX Element</h1>
+        <p>Test</p>
+    </>
+)
+// JSX Element => React.createElement => Object => HTML Element
+
+console.log(JSXElement);
+ReactDOM.createRoot(document.getElementById('root')).render(JSXElement);
