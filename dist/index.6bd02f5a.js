3f01d48657651eeccd1fce9653cdf3545c959902
Satya <Trikasatya@Admins-MacBook-Air.local>
2024-12-01 23:38:46 +0530

    uninstalled node_modules

--- dist/index.6bd02f5a.js
+++
@@ -1,5 +0,0 @@
-// import { createRoot } from 'react-dom/client';
-const element = React.createElement('h1', {}, 'Hello, world!');
-console.log(element);
-
-//# sourceMappingURL=index.6bd02f5a.js.map
