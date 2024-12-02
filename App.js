6065422c7b28ac248ad794fdc46a6d921e60f1e0
Satya <Trikasatya@Admins-MacBook-Air.local>
2024-12-02 19:45:50 +0530

    Played with React Code

--- App.js
+++ App.js
@@ -2,16 +2,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = React.createElement('h1', {style: {color: "red"}}, 'Hello, world');
+const element = React.createElement('h1', { style: { color: "red" } }, 'Hello, world');
 console.log(element);
 
 const JSXElement = (
-    <>
-        <h1>JSX Element</h1>
-        <p>Test</p>
-    </>
+  <>
+    <h1>JSX Element</h1>
+    <p>Test</p>
+  </>
 )
-// JSX Element => React.createElement => Object => HTML Element
 
-console.log(JSXElement);
-ReactDOM.createRoot(document.getElementById('root')).render(JSXElement);
+const HomePage = () => {
+  const [data, setData] = React.useState('');
+
+  const validateData = () => {
+    setData("Data is valid");
+  }
+
+  return (
+    <div>
+      <input placeholder='Input Your Name'/>
+      <button onClick={validateData}>Submit</button>
+      <div className='hello'>
+        {data}
+      </div>
+    </div>
+  )
+}
+
+const root = document.getElementById('root');
+ReactDOM.render(<HomePage />, root);
